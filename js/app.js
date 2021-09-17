668795290eef664a893881c4fad941e32a8bc6a4
DcentLookingCat <67964470+DcentLookingCat@users.noreply.github.com>
2021-09-17 14:01:26 -0700

    Wire up Contact form

--- js/app.js
+++ js/app.js
@@ -1,20 +1,39 @@
 (function (app) {
-    app.homepage = function () {
-      
-        setCopyRightDate();
-    };
-    
-    app.portfoliopage = function () {
-        setCopyRightDate();
-    };
+  app.homepage = function () {
+      setCopyRightDate();
+      wireContactForm();
+  };
 
-    app.workitempage = function () {
-        setCopyRightDate();
-    }
+  app.portfolio = function () {
+    setCopyRightDate();
+  };
+
+  app.workItem = function () {
+    setCopyRightDate();
+  };
 
   function setCopyRightDate() {
     const date = new Date();
     document.getElementById('copyrightYear').innerText = date.getFullYear();
+  }
+  
+    function wireContactForm() {
+        const contactForm = document.getElementById('contact-form');
+        contactForm.onsubmit = contactFormSubmit;
     }
-    
-})(window.app = window.app || {});
+
+    function contactFormSubmit(e) {
+        e.preventDefault();
+
+        const contactForm = document.getElementById('contact-form');
+        const name = contactForm.querySelector('#name');
+        const email = contactForm.querySelector('#email');
+        const message = contactForm.querySelector('#message');
+
+        const mailTo = `mailto:jrochford357@gmail.com?subject=Contact From ${name.value}&body=${message.value}`;
+        window.open(mailTo);
+        name.value = '';
+        email.value = '';
+        message.value = '';
+    }
+})((window.app = window.app || {}));
