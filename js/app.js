6ea9a25aed52d2174527441edb597ea8a3c8df71
DcentLookingCat <67964470+DcentLookingCat@users.noreply.github.com>
2021-09-21 15:32:35 -0700

    Setup Dynamic Work Items

--- js/app.js
+++ js/app.js
@@ -1,4 +1,8 @@
 (function (app) {
+    app.portfolioItems = [];
+    app.selectedItem = {};
+
+
   app.homepage = function () {
       setCopyRightDate();
       wireContactForm();
@@ -9,7 +13,10 @@
   };
 
   app.workItem = function () {
-    setCopyRightDate();
+      setCopyRightDate();
+      loadPageData();
+      loadSpecificItem();
+      setWorkItem();
   };
 
   function setCopyRightDate() {
@@ -36,4 +43,176 @@
         email.value = '';
         message.value = '';
     }
+
+    async function loadPageData() {
+        const cacheData = sessionStorage.getItem('site-data');
+
+        if (cacheData !== null) {
+            app.portfolioItems = JSON.parse(cacheData);
+        } else {
+            const rawData = await fetch('sitedata.json');
+            const data = await rawData.json();
+            app.portfolioItems = data;
+            sessionStorage.setItem('site-data', JSON.stringify(data));
+        }
+    }
+
+    function loadSpecificItem() {
+        const params = new URLSearchParams(window.location.search);
+        let item = Number.parseInt(params.get('item'));
+
+        if (item > app.portfolioItems.length || item < 1) {
+            item = 1;
+        }
+
+        app.selectedItem = app.portfolioItems[item - 1];
+        app.selectedItem.id = item;
+    }
+
+
+    function setWorkItem() {
+        const header = document.getElementById('work-item-header');
+        header.innerText = `0${app.selectedItem.id}. ${app.selectedItem.title}`;
+
+        const image = document.getElementById('work-item-image');
+        image.src = app.selectedItem.largeImage;
+        image.alt = app.selectedItem.largeImageAlt;
+
+        const projectText = document.querySelector('#project-text p');
+        projectText.innerText = app.selectedItem.projectText;
+
+        const originalTechnologiesList = document.querySelector('#technologies-list ul')
+        const technologiesSection = document.getElementById('technologies-list');
+        const ul = document.createElement('ul');
+
+        app.selectedItem.technologiesList.forEach(el => {
+            const li = document.createElement('li');
+            li.innerText = el;
+            ul.appendChild(li);
+        })
+        originalTechnologiesList.remove();
+        technologiesSection.appendChild(ul);
+    }
+
+
+
+
+
+
+    // async function loadPageData() {
+    //     const cacheData = sessionStorage.getItem('site-data');
+
+    //     if (cacheData !== null) {
+    //         app.portfolioItems = JSON.parse(cacheData);
+    //     } else {
+    //         const rawData = await fetch('sitedata.json');
+    //         const data = await rawData.json();
+    //         app.portfolioItems = data;
+    //         sessionStorage.setItem('site-data', JSON.stringify(data));
+    //     }
+
+    // }
+
+    // function loadSpecificItem() {
+    //     const params = new URLSearchParams(window.location.search);
+    //     let item = Number.parseInt(params.get('item'));
+
+    //     if (item > app.portfolioItems.length || item < 1) {
+    //         item = 1;
+    //     }
+
+    //     app.selectedItem = app.portfolioItems[item - 1];
+    //     app.selectedItem.id = item;
+    // }
+
+    // function setWorkItem() {
+    //     const header = document.getElementById('work-item-header');
+    //     header.innerText = `0${app.selectedItem.id}. ${app.selectedItem.title}`;
+
+    //     const image = document.getElementById('work-item-image');
+    //     image.src = app.selectedItem.largeImage;
+    //     image.alt = app.selectedItem.largeImageAlt;
+
+    //     const projectText = document.querySelector('#project-text p');
+    //     projectText.innerText = app.selectedItem.projectText;
+
+    //     const originalTechnologiesList = document.querySelector('#technologies-list ul');
+    //     const technologiesSection = document.getElementById('technologies-list');
+    //     const ul = document.createElement('ul');
+
+    //     app.selectedItem.technologiesList.forEach(el => {
+    //         const li = document.createElement('li');
+    //         li.innerText = el;
+    //         ul.appendChild(li);
+    //     });
+
+    //     originalTechnologiesList.remove();
+    //     technologiesSection.appendChild(ul);
+
+    //     const challengesText = document.querySelector('#challenges-text p');
+    //     challengesText.innerText = app.selectedItem.challengesText;
+    // }
+
+
+
+
+
+
+
+    // async function loadPageData() {
+    //     const cacheData = sessionStorage.getItem('site-data');
+        
+    //     if (cacheData !== null) {
+    //         app.portfolioItems = JSON.parse(cacheData);
+    //     } else {
+    //         const rawData = await fetch('sitedata.json');
+    //         const data = await rawData.json();
+    //         app.portfolioItems = data;
+    //         sessionStorage.setItem('site-data', JSON.stringify(data));
+    //     }
+        
+    // }
+    
+    // function loadSpecificItem() {
+    //     const params = new URLSearchParams(window.location.search);
+    //     let item = Number.parseInt(params.get('item'));
+        
+    //     if (item > app.portfolioItems.length || item < 1) {
+    //         item = 1;
+    //     }
+        
+    //     app.selectedItem = app.portfolioItems[item - 1];
+    //     app.selectedItem.id = item;
+    // }
+    
+    // function setWorkItem() {
+
+    //     const header = document.getElementById('work-item-header');
+    //     header.innerText = `0${app.selectedItem.id}. ${app.selectedItem.title}`;
+
+    //     const image = document.getElementById('work-item-image');
+    //     image.src = app.selectedItem.largeImage;
+    //     image.alt = app.selectedItem.largeImageAlt;
+        
+    //     const projectText = document.querySelector('#project-text p');
+    //     projectText.innerText = app.selectedItem.projectText;
+        
+    //     const originalTechnologiesList = document.querySelector('#technologies-list ul');
+    //     const technologiesSection = document.getElementById('technologies-list');
+    //     const ul = document.createElement('ul');
+
+    //     app.selectedItem.technologiesList.forEach(el => {
+    //         const li = document.createElement('li');
+    //         li.innerText = el;
+    //         ul.appendChild(li);
+    //     })
+
+    //     originalTechnologiesList.remove();
+    //     technologiesSection.appendChild(ul);
+
+    //     const challengesText = document.querySelector('#challenges-text p');
+    //     challengesText.innerText = app.selectedItem.challengesText;
+    // }
+    
+
 })((window.app = window.app || {}));
